30e78a5e8c29f972a33e06faee4a498e5dfd7a35
kty2001 <tyyoujungzz@naver.com>
2024-06-04 23:16:07 +0900

    add schedulera and autobatch and amp

--- pytorch-course/object-detection/global-wheat-detection/train.py
+++ pytorch-course/object-detection/global-wheat-detection/train.py
@@ -1,7 +1,10 @@
 import argparse
 import os
 import random
-import shutil\
+import shutil
+import wandb
+
+import numpy as np
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -12,15 +15,10 @@ from torch import optim
 from torch.utils.data import DataLoader
 import torchvision
 
-from src.dataset import WheatDataset, collate_fn
+from src.dataset import WheatDataset, collate_fn, get_transform
 from src.model import DetectionModel
-from src.utils import split_dataset, MeanAveragePrecision
+from src.utils import split_dataset, MeanAveragePrecision, check_train_batch_size
 
-import numpy as np
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
-
-import wandb
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--device", default="cpu", type=str, help="device to use for training")
@@ -35,37 +33,32 @@ def denormalize_image(image):
     mean = np.array((0.485, 0.456, 0.406))
     std = np.array((0.229, 0.224, 0.225))
     image = image * std.reshape((1, 3, 1, 1)) + mean.reshape((1, 3, 1, 1))
+    image = image * 255.0
     image = np.clip(image, 0, 1)
     return image
 
 
-def visualize_dataset(image_dir: os.PathLike, csv_path: os.PathLike, bbox_params: A.BboxParams, image_size: int, save_dir: os.PathLike, n_images: int = 10) -> None:
+def visualize_dataset(image_dir: os.PathLike, csv_path: os.PathLike, image_size: int, state: str, save_dir: os.PathLike, n_images: int = 10) -> None:
     """데이터셋 샘플 bbox 그려서 시각화
     
     :param save_dir: bbox 그린 그림 저장할 폴더 경로
     :type save_dir: os.PathLike
     """
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    else:
+    if os.path.exists(save_dir):
         shutil.rmtree(save_dir)
-        os.makedirs(save_dir)
+    os.makedirs(save_dir)
 
     dataset = WheatDataset(
         image_dir=image_dir,
         csv_path=csv_path,
-        transform=A.Compose([
-            A.Rotate(limit=(-30, 30), p=0.5),
-            A.HorizontalFlip(p=0.5),
-            A.RandomBrightnessContrast(p=0.2),
-            A.Resize(width=image_size, height=image_size),
-            ToTensorV2()
-        ], bbox_params=bbox_params)
+        transform=get_transform(state=state, image_size=image_size)
     )
 
     indexes = random.choices(range(len(dataset)), k=n_images)
     for i in indexes:
         image, target, image_id = dataset[i]
+        if state == 'train':
+            image = denormalize_image(image)[0]
         image = image.numpy().transpose(1, 2, 0)
 
         plt.imshow(image)
@@ -105,6 +98,7 @@ def train_one_epoch(
     dataloader: DataLoader,
     device: str,
     model: torchvision.models.detection,
+    scheduler: torch.optim.lr_scheduler.OneCycleLR,
     optimizer: torch.optim.Optimizer
     ) -> None:
     """Wheat 데이터셋으로 뉴럴 네트워크를 훈련합니다.
@@ -120,22 +114,30 @@ def train_one_epoch(
     """
     size = len(dataloader.dataset)
     model.train()
+
+    scaler = torch.cuda.amp.GradScaler()
     for batch, (images, targets, image_ids) in enumerate(dataloader):
         images = [image.to(device) for image in images]
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        model.train()
-        loss_dict = model(images, targets)
-        loss = sum(loss for loss in loss_dict.values())
+        with torch.cuda.amp.autocast():
+            loss_dict = model(images, targets)
+            loss = sum(loss for loss in loss_dict.values())
 
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        scaler.scale(loss).backward()
+        scaler.unscale_(optimizer)
+        scale_before = scaler.get_scale()
+        scaler.step(optimizer)
+        scaler.update()    
+        scale_after = scaler.get_scale()
+        if scale_before <= scale_after:
+                scheduler.step()
         if batch % 10 == 0:
             if batch == 0:
                 wandb.log({
-                    "noramlized_train_image": [wandb.Image(images[i], caption=f"{image_ids[i]}.jpg") for i in range(len(images))],
-                    "origin_train_image": [wandb.Image(denormalize_image(images[i].cpu()), caption=f"{image_ids[i]}.jpg") for i in range(len(images))],
+                    "noramlized_train_batch": [wandb.Image(images[i], caption=f"{image_ids[i]}.jpg") for i in range(len(images))],
+                    "origin_train_batch": [wandb.Image(denormalize_image(images[i].cpu()), caption=f"{image_ids[i]}.jpg") for i in range(len(images))],
                 })
             wandb.log({
                 "total_loss": loss.item(),
@@ -185,21 +187,22 @@ def val_one_epoch(
     test_rpn_loss = 0
     with torch.no_grad():
         for images, targets, image_ids in dataloader:
-            images = [image.to(device, dtype=torch.float32) for image in images]
+            images = [image.to(device) for image in images]
             targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+        
+            with torch.cuda.amp.autocast():
+                model.train()
+                loss_dict = model(images, targets)
+                loss = sum(loss for loss in loss_dict.values())
 
-            model.train()
-            loss_dict = model(images, targets)
-            loss = sum(loss for loss in loss_dict.values())
+                test_loss += loss
+                test_cls_loss += loss_dict['loss_classifier']
+                test_box_loss += loss_dict['loss_box_reg']
+                test_obj_loss += loss_dict['loss_objectness']
+                test_rpn_loss += loss_dict['loss_rpn_box_reg']
 
-            test_loss += loss
-            test_cls_loss += loss_dict['loss_classifier']
-            test_box_loss += loss_dict['loss_box_reg']
-            test_obj_loss += loss_dict['loss_objectness']
-            test_rpn_loss += loss_dict['loss_rpn_box_reg']
-
-            model.eval()
-            preds = model(images)
+                model.eval()
+                preds = model(images)
 
             metric.update(preds, image_ids, image_size)
     test_loss /= num_batches
@@ -208,8 +211,8 @@ def val_one_epoch(
     test_obj_loss /= num_batches
     test_rpn_loss /= num_batches
     iou_score = metric.compute()
-    print(f'\nTest Error: \n Avg loss: {test_loss:>8f} \n Class loss: {test_cls_loss:>8f} \n Box loss: {test_box_loss:>8f} \n Obj loss: {test_obj_loss:>8f} \n RPN loss: {test_rpn_loss:>8f} \n')
 
+    print(f'\nTest Error: \n Avg loss: {test_loss:>8f} \n Class loss: {test_cls_loss:>8f} \n Box loss: {test_box_loss:>8f} \n Obj loss: {test_obj_loss:>8f} \n RPN loss: {test_rpn_loss:>8f} \n')
     wandb.log({
         "AP [ IoU=0.50:0.95 | area=all | maxDets=100 ]": iou_score[0],
         "AP [ IoU=0.50      | area=all | maxDets=100 ]": iou_score[1],
@@ -225,7 +228,7 @@ def val_one_epoch(
         "AR [ IoU=0.50:0.95 | area=  l | maxDets=100 ]": iou_score[11],
     })
     wandb.log({
-        "test_image": [wandb.Image(images[i].cpu(), caption=f"{image_ids[i]}.jpg") for i in range(len(images))],
+        "inference_batch": [wandb.Image(images[i].cpu(), caption=f"{image_ids[i]}.jpg") for i in range(len(images))],
         "test_loss": test_loss,
         "test_cls_loss": test_cls_loss,
         "test_box_loss": test_box_loss,
@@ -267,49 +270,38 @@ def train(
     train_image_dir = 'data/global-wheat-detection/train'
     train_csv_path = 'data/global-wheat-detection/train_answer.csv'
     test_csv_path = 'data/global-wheat-detection/test_answer.csv'
-
-    bbox_params = A.BboxParams(
-                format='coco',
-                label_fields=['labels']
-    )
     
     num_classes = 1
     split_dataset(csv_path)
-    visualize_dataset(train_image_dir, train_csv_path, bbox_params, image_size, save_dir='examples/global-wheat-detection/train')
-    visualize_dataset(train_image_dir, test_csv_path, bbox_params, image_size, save_dir='examples/global-wheat-detection/test')
+    visualize_dataset(train_image_dir, train_csv_path, image_size, 'train', save_dir='examples/global-wheat-detection/train')
+    visualize_dataset(train_image_dir, test_csv_path, image_size, 'test', save_dir='examples/global-wheat-detection/test')
 
     training_data = WheatDataset(
         image_dir=train_image_dir,
         csv_path=train_csv_path,
-        transform=A.Compose([
-            A.Rotate(limit=(-30, 30), p=0.5),
-            A.HorizontalFlip(p=0.5),
-            A.RandomBrightnessContrast(p=0.2),
-            A.Resize(width=image_size, height=image_size),
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-            ToTensorV2(),
-        ], bbox_params=bbox_params),
+        transform=get_transform(state='train', image_size=image_size),
     )
     test_data = WheatDataset(
         image_dir=train_image_dir,
         csv_path=test_csv_path,
-        transform = A.Compose([
-            A.Resize(width=image_size, height=image_size),
-            ToTensorV2()
-        ], bbox_params=bbox_params),
+        transform=get_transform(state='test', image_size=image_size),
     )
 
-    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True, num_workers=0, collate_fn=collate_fn)
-    test_dataloader = DataLoader(test_data, batch_size=int(batch_size/2), num_workers=0, collate_fn=collate_fn)
-    
     model = DetectionModel(num_classes=num_classes).make_model().to(device)
+
+    # check proper batch size
+    batch_size = check_train_batch_size(model=model, imgsz=image_size)
+
+    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True, num_workers=0, collate_fn=collate_fn)
+    test_dataloader = DataLoader(test_data, batch_size=int(batch_size/4), num_workers=0, collate_fn=collate_fn)
     
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=0.005)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.1, steps_per_epoch=len(train_dataloader), epochs=epochs)
     metric = MeanAveragePrecision(csv_path=csv_path)
 
     for t in range(epochs):
         print(f'Epoch {t+1}\n-------------------------------')
-        train_one_epoch(train_dataloader, device, model, optimizer)
+        train_one_epoch(train_dataloader, device, model, scheduler, optimizer)
         val_one_epoch(test_dataloader, device, model, image_size, metric)
     print('Done!')
 
