8977f704d1d9eb1ea7af4a3bc44b703531cc5882
Seunghwan0828 <jshwan0828@naver.com>
2024-10-31 03:20:15 +0900

    Imagenet lightning code update

---
+++ pytorch-course/classification/imagenet/lightning/train.py
@@ -0,0 +1,123 @@
+import os
+import argparse
+
+import numpy as np
+import torch
+from torch import nn
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
+from lightning.pytorch.loggers import WandbLogger
+
+from src.dataset import ImageNetDataModule
+from src.utils import rename_dir
+from src.model import create_model
+
+
+class ClassficationModel(L.LightningModule):
+    def __init__(self, model, batch_size=32):
+        super().__init__()
+        self.model = model
+        self.batch_size = batch_size
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.losses = []
+        self.labels = []
+        self.predictions = []
+        self.save_hyperparameters()
+
+    def forward(self, inputs):
+        return self.model(inputs)
+
+    def training_step(self, batch, batch_idx):
+        inputs, target = batch
+        output = self.model(inputs)
+        loss = self.loss_fn(output, target)
+        # self.log_dict({"train_loss": loss}, on_step=True, prog_bar=True, batch_size=self.batch_size)
+        self.log('train_loss', loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        inputs, target = batch
+        output = self.model(inputs)
+        loss =  self.loss_fn(output, target)
+        _, predictions = torch.max(output, 1)
+
+        target_np = target.detach().cpu().numpy()
+        predict_np = predictions.detach().cpu().numpy()
+        
+        self.losses.append(loss)
+        self.labels.append(np.int16(target_np))
+        self.predictions.append(np.int16(predict_np))
+        self.log('valid_loss', loss)
+        return loss
+    
+    def on_validation_epoch_end(self):
+        labels = np.concatenate(np.array(self.labels, dtype=object))
+        predictions = np.concatenate(np.array(self.predictions, dtype=object))
+        acc = sum(labels == predictions)/len(labels)
+
+        labels = labels.tolist()
+        predictions = predictions.tolist()
+        loss = sum(self.losses)/len(self.losses)
+        self.log('val_acc', acc)
+        self.log('val_loss', loss)
+        # self.log_dict({"val_acc": acc, "val_loss": loss}, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        
+        self.losses.clear()
+        self.labels.clear()
+        self.predictions.clear()
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.model.parameters(), lr=1e-3, momentum=0.9)
+    
+
+def main(classification_model, data, batch, epoch, save_path, device, gpus):
+    rename_dir()
+    model = ClassficationModel(create_model(classification_model))
+    imagenet = ImageNetDataModule(data, batch)
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    if len(gpus) == 1:
+        gpus = [int(gpus)]
+    else:
+        gpus = list(map(int, gpus.split(',')))
+
+    
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_acc',
+        mode='max',
+        dirpath= f'{save_path}',
+        filename= f'{classification_model}-'+'{epoch:02d}-{val_acc:.2f}',
+        save_top_k=1,
+    )
+    early_stopping = EarlyStopping(
+        monitor='val_acc',
+        mode='max',
+        patience=10
+    )
+    wandb_logger = WandbLogger(project="ImageNet")
+    trainer = L.Trainer(
+        accelerator=device,
+        devices=gpus,
+        max_epochs=epoch,
+        precision='16-mixed',
+        logger=wandb_logger,
+        callbacks=[checkpoint_callback, early_stopping],
+    )
+    trainer.fit(model, imagenet)
+    trainer.test(model, imagenet)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', type=str, default='resnet')
+    parser.add_argument('-b', '--batch_size', dest='batch', type=int, default=32)
+    parser.add_argument('-e', '--epoch', type=int, default=50)
+    parser.add_argument('-d', '--data_path', dest='data', type=str, default='../dataset')
+    parser.add_argument('-s', '--save_path', dest='save', type=str, default='./checkpoint/')
+    parser.add_argument('-dc', '--device', type=str, default='gpu')
+    parser.add_argument('-g', '--gpus', type=str, nargs='+', default='0')
+    args = parser.parse_args()
+    
+    main(args.model, args.data, args.batch, args.epoch, args.save, args.device, args.gpus)
