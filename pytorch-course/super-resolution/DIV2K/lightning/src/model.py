35b48a528d75d0375d6853b53d6f8d93ff04ef32
zrooo <kdofutu@gmail.com>
2024-12-26 09:41:36 +0000

    Merge Super-Resolution

--- pytorch-course/super-resolution/DIV2K/lightning/src/model.py
+++ pytorch-course/super-resolution/DIV2K/lightning/src/model.py
@@ -1,46 +1,52 @@
 import torch
 
-from basicsr.archs.rrdbnet_arch import RRDBNet
+# from basicsr.archs.rrdbnet_arch import RRDBNet
 from basicsr.archs.edsr_arch import EDSR
+from basicsr.archs.srvgg_arch import SRVGGNetCompact
+from basicsr.archs.discriminator_arch import UNetDiscriminatorSN
 
 
-def create_model(model: str, weight_path: str | tuple, upscale: int):   # pretrained_path: args.weights
-    if model.lower() == 'esrgan':
-        model = RRDBNet(
-            num_in_ch=3,  # RGB
-            num_out_ch=3, # RGB
-        )
-        if weight_path:  # 'weights/ESRGAN.pth'
-            weights = torch.load(weight_path, weights_only=True)    # .pth 파일 안의 가중치 데이터만 로드, 실행 가능한 객체 무시 (unpickle의 잠재적인 보안 문제를 예방)
-            if 'params' in weights:
-                weights = weights['params']
-            model.load_state_dict(weights, strict=False)
-
-    elif model.lower() == 'realesrgan':
-        model = RRDBNet(
-            num_in_ch=12,   # 3->12
-            num_out_ch=3,
-            scale=upscale,
-            num_feat=64,
-            num_block=23,
-            num_grow_ch=32,
+def create_model(model_type: str, weight_path: str | tuple, upscale: int):   # pretrained_path: args.weights
+    '''
+    if model_type.lower() == 'realesrgan':
+        generator = SRVGGNetCompact(
+                    num_in_ch = 3,
+                    num_out_ch = 3,
+                    num_feat = 64,
+                    num_conv = 16,
+                    upscale = 4,
+                    act_type = 'prelu'
         )
         if weight_path:
-            weights = torch.load(weight_path, weights_only=True)
+            weights = torch.load(weight_path, weights_only=True)     # .pth 파일 안의 가중치 데이터만 로드, 실행 가능한 객체 무시 (unpickle의 잠재적인 보안 문제를 예방)
             if 'params' in weights:
                 weights = weights['params']
-            model.load_state_dict(weights, strict=False)
+            generator.load_state_dict(weights, strict=False)
 
-    elif model.lower() == 'edsr':
+        discriminator = UNetDiscriminatorSN(
+                        num_in_ch = 3,
+                        num_feat = 64,
+                        skip_connection = True
+        )
+        # if weight_path: 
+        #     weights = torch.load(weight_path, weights_only=True)    
+        #     if 'params' in weights:
+        #         weights = weights['params']
+        #     discriminator.load_state_dict(weights, strict=False)
+
+        return generator, discriminator
+    '''
+
+    if model_type.lower() == 'edsr':
         model = EDSR(
-                num_in_ch=3,
-                num_out_ch=3,
-                num_feat=64,
-                num_block=16,
-                upscale=upscale,
-                res_scale=1,
-                img_range=255.,
-                rgb_mean=(0.4488, 0.4371, 0.4040)
+                num_in_ch = 3,
+                num_out_ch = 3,
+                num_feat = 64,
+                num_block = 16,
+                upscale = upscale,
+                res_scale = 1,
+                img_range = 255.,
+                rgb_mean = (0.4488, 0.4371, 0.4040)
             )
         if weight_path:
             weights = torch.load(weight_path, weights_only=True)
@@ -48,7 +54,8 @@ def create_model(model: str, weight_path: str | tuple, upscale: int):   # pretra
                 weights = weights['params']
             model.load_state_dict(weights, strict=False)
 
+        return model
+
     for param in model.parameters():
         param.requires_grad = True
-
-    return model
+        
